fe71ffcf1137b7dc0d1ab2b662436809db0b6db5
Unknown <juliano.s.nakamura@hotmail.com>
2019-08-08 16:15:21 -0300

    Basic panels

---
+++ src/com/dbserver/MainFrame.java
@@ -0,0 +1,10 @@
+package com.dbserver;
+
+import javax.swing.*;
+
+public class MainFrame {
+    private JButton resultButton;
+    private JButton votingButton;
+    private JPanel mainFrame;
+    private JLabel resultLabel;
+}
---
+++ src/com/dbserver/Voter.java
@@ -0,0 +1,4 @@
+package com.dbserver;
+
+public class Voter {
+}
