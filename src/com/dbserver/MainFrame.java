2f0c369449ccf94744966dc71d9d639eeb5ae96d
Unknown <juliano.s.nakamura@hotmail.com>
2019-08-08 16:40:12 -0300

    Changing panels

--- src/com/dbserver/MainFrame.java
+++ src/com/dbserver/MainFrame.java
@@ -1,10 +1,39 @@
 package com.dbserver;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame {
     private JButton resultButton;
     private JButton votingButton;
     private JPanel mainFrame;
     private JLabel resultLabel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainFrame");
+        frame.setContentPane(new MainFrame().mainFrame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MainFrame() {
+
+        votingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Voter newVotingForm = new Voter();
+                newVotingForm.votinPanel();
+            }
+        });
+
+        resultButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- src/com/dbserver/Voter.java
+++ src/com/dbserver/Voter.java
@@ -1,4 +1,15 @@
 package com.dbserver;
 
+import javax.swing.*;
+
 public class Voter {
+    private JPanel votingPanel;
+
+    public static void votinPanel() {
+        JFrame frame = new JFrame("Voter");
+        frame.setContentPane(new Voter().votingPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
